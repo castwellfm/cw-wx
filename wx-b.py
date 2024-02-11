3b7d693af12a590d7ff4ba242efb1828e79678e3
castwellfm <153681595+castwellfm@users.noreply.github.com>
2024-02-11 23:43:52 +0200

    Update wx-b.py

--- wx-b.py
+++ wx-b.py
@@ -1,6 +1,8 @@
 import urllib.request
 import whisperx
 import gc 
+import os
+os.environ['TRANSFORMERS_CACHE'] = '/root/tf_cache/'
 device = "cuda" 
 batch_size = 16 # reduce if low on GPU mem
 compute_type = "float16" # change to "int8" if low on GPU mem (may reduce accuracy)
